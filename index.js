defbefabbeeded3047194e9aba5c1b7676091755
Bhavika <112804763+Bhavikagire@users.noreply.github.com>
2023-02-20 18:47:07 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const express = require("express")
 require("dotenv").config()
-const app = express()
-app.use(express.json())
-
-const cors = require("cors")
-app.use(cors())
-
 const {userRouter} = require("./routers/userroute")
-app.use("/users",userRouter)
+const {authenticate}=require("./middleware/authenticatemid")
+const{postRouter}= require("./routers/postroute")
+const cors = require("cors")
 const {connection}=require("./db")
 
+const app = express()
+
+app.use(express.json())
+app.use(cors())
 
 
-const {authenticate}=require("./middleware/authenticatemid")
-app.use(authenticate)
-const{postRouter}= require("./routers/postroute")
-app.use("/post",postRouter)
+
 app.get("/",(req,res)=>{
     res.send("welcome to full stack app")
 })
+
+
+app.use("/users",userRouter)
+
+app.use(authenticate)
+
+app.use("/post",postRouter)
+
 
 app.listen(process.env.PORT,async()=>{
     try {
